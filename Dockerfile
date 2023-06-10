86bb858cacf6879223e586aef65ed5d25c9f3e40
Luiz Filipe <luiz.filipe@neomed.tech>
2023-06-10 11:40:44 -0300

    Alterando aplicação

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
-# Usa a imagem oficial do Node.js como base
+# Use a imagem base do Node.js
 FROM node:14
 
-# Define o diretório de trabalho dentro do contêiner
+# Defina o diretório de trabalho dentro do contêiner
 WORKDIR /app
 
-# Copia todos os arquivos do diretório atual para o diretório de trabalho
+# Copie os arquivos do aplicativo para o diretório de trabalho
 COPY . .
 
-# Instala as dependências do projeto
+# Instale as dependências
 RUN npm install
 
-# Expõe a porta em que a aplicação está rodando
-EXPOSE 3000
+# Expõe a porta 80
+EXPOSE 80
 
-# Comando para executar a aplicação quando o contêiner for iniciado
+# Inicie o aplicativo
 CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -1,29 +1,11 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+import express from 'express'
+import { generateUploadURL } from './s3.js'
 
-const port = 3000;
+const app = express()
 
-app.get('/', (req, res) => {
-  res.send('O backend está funcionando');
-});
+app.get('/api', async (req, res) => {
+  const url = await generateUploadURL()
+  res.send({url})
+})
 
-// Manipula a conexão do socket
-io.on('connection', (socket) => {
-  console.log('Cliente conectado');
-
-  // Manipula o evento para enviar a mensagem ao frontend
-  socket.on('sendMessage', (data) => {
-    io.emit('message', data); // Envia a mensagem para todos os clientes conectados
-  });
-
-  // Manipula o evento de desconexão
-  socket.on('disconnect', () => {
-    console.log('Cliente desconectado');
-  });
-});
-
-http.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
+app.listen(80, () => console.log("listening on port 80"))
