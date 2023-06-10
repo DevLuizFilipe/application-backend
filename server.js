98f235c15626651bf93220f0e935ccffa999d659
Luiz Filipe <luiz.filipe@neomed.tech>
2023-06-10 12:21:48 -0300

    fix: corrigindo e aplicando alterações

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ import { generateUploadURL } from './s3.js'
 
 const app = express()
 
-app.get('/api', async (req, res) => {
-  const url = await generateUploadURL()
-  res.send({url})
+app.get('/', (req, res) => {
+  res.send('OK')
 })
 
-app.listen(80, () => console.log("listening on port 80"))
+app.get('/api', async (req, res) => {
+  const url = await generateUploadURL()
+  res.send({ url })
+})
+
+app.listen(80, () => console.log("Ouvindo na porta 80"))
