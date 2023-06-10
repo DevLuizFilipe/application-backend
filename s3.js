55a0e213ad3a00ca16a0dab7d5bcb73696417920
Luiz Filipe <luiz.filipe@neomed.tech>
2023-06-10 11:59:23 -0300

    corrigindo sintaxe

--- s3.js
+++ s3.js
@@ -8,8 +8,8 @@ dotenv.config()
 
 const region = process.env.AWS_REGION
 const bucketName = process.env.AWS_BUCKET_NAME
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID || process.env.GITHUB_SECRET_AWS_ACCESS_KEY_ID
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY || process.env.GITHUB_SECRET_AWS_SECRET_ACCESS_KEY
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
 
 const s3 = new aws.S3({
   region,
