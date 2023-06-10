1489e64ab98281dd87e54696a0cabdfeb41b017b
Luiz Filipe <luiz.filipe@neomed.tech>
2023-06-10 12:01:59 -0300

    retirando process env

--- s3.js
+++ s3.js
@@ -6,10 +6,10 @@ const randomBytes = promisify(crypto.randomBytes)
 
 dotenv.config()
 
-const region = process.env.AWS_REGION
-const bucketName = process.env.AWS_BUCKET_NAME
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+const region = AWS_REGION
+const bucketName = AWS_BUCKET_NAME
+const accessKeyId = AWS_ACCESS_KEY_ID
+const secretAccessKey = AWS_SECRET_ACCESS_KEY
 
 const s3 = new aws.S3({
   region,
